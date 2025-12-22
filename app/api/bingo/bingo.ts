60c93f76e4b027d554bfa369262c39b5e487fa5a
Luis Martinez Kowalski <luis.martinez@taxfix.de>
2025-12-22 12:25:40 +0100

    add bingo.ts

---
+++ app/api/bingo/bingo.ts
@@ -0,0 +1,24 @@
+const people = [
+  "The President of",
+  "The Vice President of",
+  "A Corporate Executive",
+  "An olympic Athlete",
+  "An actor/actress",
+  "A musician",
+  "A scientist",
+  "A technology entrepreneur",
+  "A movie director",
+];
+
+const companies = ["Coca cola", "Sony Entertainment", "a VC Firm"]
+
+const actions = [
+  "wins an Oscar",
+  "wins a Grammy",
+  "wins a Nobel Prize",
+  "overdoses on ketamine",
+  "gets arrested for driving under the influence",
+  "gets arrested for possession of a weapon",
+  "gets arrested for possession of a controlled substance",
+  "gets arrested for possession of a controlled substance",
+]
